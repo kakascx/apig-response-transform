77af7dd24c1d8316b243ce00680cd457413298dd
sunchuxiong <sunchuxiong@inspur.com>
2019-06-24 20:14:35 +0800

    add annotation and rename some variable

--- kong/plugins/apig-response-transform/handler.lua
+++ kong/plugins/apig-response-transform/handler.lua
@@ -13,13 +13,17 @@ end
 
 function TransferPlugin:access(config)
   TransferPlugin.super.access(self)
+  --获取请求参数中的format参数
   format = kong.request.get_query_arg("Format")
 end
 
 function TransferPlugin:header_filter(config)
   TransferPlugin.super.header_filter(self)
+  --获取response header中的content-type和request-id
   contentType = kong.response.get_header("Content-Type")
   requestId = kong.response.get_header("Request-Id")
+  --format参数为xml，需要转换为json，默认为xml
+  --如果修改为json，需要清空content-length，因为修改了长度
   if format == "xml" or format == nil then
 	if string.find(contentType,"application/json") then
 	kong.response.set_header("Content_Type","application/xml")
@@ -30,26 +34,33 @@ end
 
 function TransferPlugin:body_filter(config)
   TransferPlugin.super.body_filter(self)
+  --如果format参数为xml或null，同时响应body为json格式，进行转换
   if format == "xml" or format == nil then
-     if string.find(contentType,"application/json") then		
+     if string.find(contentType,"application/json") then
+     --ngx.arg[1]为响应body，ngx.arg[2]为body结束标志位
 	local chunk, eof = ngx.arg[1], ngx.arg[2]
 	local buffered = ngx.ctx.buffered
 	if not buffered then 
 		buffered = {}
 		ngx.ctx.buffered = buffered
 	end
+	--将所有buffer拼接起来
 	if chunk ~= "" then
 	buffered[#buffered+1] = chunk
 	ngx.arg[1] = nil
 	end
+	--如果标志位为true说明body全部接收
 	if eof then
 	local whole = table.concat(buffered)
+	--将json格式的body解析为lua table
 	   whole = cjson.decode(whole)
+	--如果响应body为数组形式，将body包一层data并在data同级加一个requestid的键值对   
 	   if json2xml.isArrayTable(whole) then
 	      local m={}
 	      m["requestId"]=requestId
 	      m["data"]=whole
 	      whole = json2xml.toxml(m)
+	--如果为json对象，直接给body添加一个requestid的键值对   	  
 	   else
 	      whole["requestId"]=requestId
 	      whole = json2xml.toxml(whole)
--- kong/plugins/apig-response-transform/json2xml.lua
+++ kong/plugins/apig-response-transform/json2xml.lua
@@ -2,11 +2,13 @@
 
 local M = {}
 
-local indentation = ""
+local indentation = "" --缩进
 local xml = ""
 local str = ""
 
---if table is an array
+---isArrayTable
+---@param t table 判断是否为数组类型的table
+---@return boolean 数组类型返回true，否则返回false
 function M.isArrayTable(t)
     if type(t) ~= "table" then
         return false
@@ -26,14 +28,21 @@ function M.isArrayTable(t)
     return true
 end
 
---transfer json to xml
-function M.toxml(table)
-    ProcessResponseBody(table)
+
+
+---toxml 对json格式进行转换
+---@param table table 由响应body转化而来的table数据
+---@return string 返回xml格式数据
+function M.toxml(tblBody)
+    ProcessResponseBody(tblBody)
     local result = xml
     xml=""
     return result
 end
 
+---ProcessResponseBody 核心转换函数，通过递归将json转换为xml
+---@param value table 需要转换的table格式的数据
+---@return nil 动态更新xml字符串
 function ProcessResponseBody(value)
     if type(value) == "string" then
         str = str .. indentation ..value .. "\n"
@@ -41,19 +50,19 @@ function ProcessResponseBody(value)
         str = " "
     elseif type(value) == "table" then
         for name,data in pairs(value) do
+            --如果key为字符串，也就是说是json对象
             if type(name) == "string" then
                 if type(data) == "string" then
-                    --indentation = indentation .. "\t"
                     str = str .. indentation .."<" .. name .. ">" .. data .. "</" .. name .. ">\n"
                     xml = xml .. str
                     str = ""
 
                 elseif type(data) == "number" or type(data) == "boolean" then
-                    --indentation = indentation .. "\t"
                     str =  str .. indentation .. "<" .. name .. ">" .. tostring(data) .. "</" .. name .. ">\n"
                     xml = xml .. str
                     str = ""
                 elseif type(data) == "table" then
+                    --如果是数组类型的table，则遍历table并给每个元素添加标签
                     if M.isArrayTable(data) then
                         for tblName,tblData in pairs(data) do
                             xml = xml .. indentation .. "<" .. name .. ">\n"
@@ -64,6 +73,7 @@ function ProcessResponseBody(value)
                             xml = xml .. str
                             str = " "
                         end
+                    --如果是json对象，加标签后对内容进行遍历
                     else
                         xml = xml .. indentation .. "<" .. name .. ">\n"
                         indentation = indentation .. "\t"
